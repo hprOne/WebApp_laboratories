b0c3d0c2095c712ee5284b57df9782391fa561a3
hprOne <bartek.laka@gmail.com>
2021-03-07 14:23:56 +0100

    Lab1

--- Lab1/main.ts
+++ Lab1/main.ts
@@ -1,8 +1,24 @@
+class Stats{
+    data1;
+    data2;
+    data3;
+    data4;
 
-function checkMakeNewInput(){
-    document.getElementById("data1")
-    document.getElementById("data2")
-    document.getElementById("data3")
-    document.getElementById("data4")
+    min;
+    max;
+    avg;
+    sum;
+    constructor(){
+        this.getElements();
+        //this.bindEvent();
+    }
+
+    getElements(){
+        this.data1 = document.querySelector('#data1')
+        this.data2 = document.querySelector('#data2')
+        this.data3 = document.querySelector('#data3')
+        this.data4 = document.querySelector('#data4')
+        //let e = a/b
+    }
 }
 
