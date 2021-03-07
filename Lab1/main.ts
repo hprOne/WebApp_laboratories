2da18ae3d31132a63f67ed20f8cc3d02b0898253
hprOne <bartek.laka@gmail.com>
2021-03-07 13:53:57 +0100

    Lab1

---
+++ Lab1/main.ts
@@ -0,0 +1,8 @@
+
+function checkMakeNewInput(){
+    document.getElementById("data1")
+    document.getElementById("data2")
+    document.getElementById("data3")
+    document.getElementById("data4")
+}
+
