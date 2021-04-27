360b915f8c8adfbe6db0808d7f0df3eb4ddd9d68
hprOne <bartek.laka@gmail.com>
2021-04-27 20:01:09 +0200

    Lab1 - Working on 4 inputs

--- Lab1/build/index.js
+++ Lab1/build/index.js
@@ -1,26 +1,36 @@
 var Stats = /** @class */ (function () {
     function Stats() {
         this.getElements();
-        this.setOutputs();
     }
     Stats.prototype.getElements = function () {
         this.dataInput1 = document.querySelector('#data1');
         this.dataInput2 = document.querySelector('#data2');
         this.dataInput3 = document.querySelector('#data3');
         this.dataInput4 = document.querySelector('#data4');
+        this.setOutputs();
     };
     Stats.prototype.setOutputs = function () {
+        var data1 = parseInt(this.dataInput1.value);
+        var data2 = parseInt(this.dataInput2.value);
+        var data3 = parseInt(this.dataInput3.value);
+        var data4 = parseInt(this.dataInput4.value);
         this.dataMin = document.querySelector('#min');
         this.dataMax = document.querySelector('#max');
         this.dataSum = document.querySelector('#sum');
         this.dataAvg = document.querySelector('#avg');
-        var checkTable = [this.dataInput1, this.dataInput2, this.dataInput3, this.dataInput4];
-        this.dataMax = Math.max.apply(Math, checkTable);
-        this.dataMin = Math.min.apply(Math, checkTable);
-        this.dataSum = this.dataInput1 + this.dataInput2 + this.dataInput3 + this.dataInput4;
-        this.dataAvg = this.dataSum / 4;
+        var checkTable = [data1, data2, data3, data4];
+        var max = Math.max.apply(Math, checkTable);
+        var min = Math.min.apply(Math, checkTable);
+        var sum = data1 + data2 + data3 + data4;
+        var avg = sum / 4;
+        this.setResult(this.dataMax, max);
+        this.setResult(this.dataMin, min);
+        this.setResult(this.dataSum, sum);
+        this.setResult(this.dataAvg, avg);
+    };
+    Stats.prototype.setResult = function (output, result) {
+        output.value = "" + result;
     };
     return Stats;
 }());
 var stats = new Stats();
-stats.run();
--- Lab1/src/index.ts
+++ Lab1/src/index.ts
@@ -1,17 +1,16 @@
 class Stats{
-    dataInput1;
-    dataInput2;
-    dataInput3;
-    dataInput4;
+    dataInput1: HTMLInputElement;
+    dataInput2: HTMLInputElement;
+    dataInput3: HTMLInputElement;
+    dataInput4: HTMLInputElement;
 
-    dataMin//: HTMLInputElement;
-    dataMax//: HTMLInputElement;
-    dataAvg//: HTMLInputElement;
-    dataSum//: HTMLInputElement;
+    dataMin: HTMLOutputElement;
+    dataMax: HTMLOutputElement;
+    dataAvg: HTMLOutputElement;
+    dataSum: HTMLOutputElement;
     
     constructor(){
         this.getElements();
-        this.setOutputs();
     }
 
     getElements(){
@@ -19,22 +18,35 @@ class Stats{
         this.dataInput2 = document.querySelector('#data2')
         this.dataInput3 = document.querySelector('#data3')
         this.dataInput4 = document.querySelector('#data4')
+        this.setOutputs();
     }
 
     setOutputs(){
+        const data1: number = parseInt(this.dataInput1.value)
+        const data2: number = parseInt(this.dataInput2.value)
+        const data3: number = parseInt(this.dataInput3.value)
+        const data4: number = parseInt(this.dataInput4.value)
+
         this.dataMin = document.querySelector('#min')
         this.dataMax = document.querySelector('#max')
         this.dataSum = document.querySelector('#sum')
-        this.dataAvg = document.querySelector('#avg')
-        
-        let checkTable: number[] = [this.dataInput1, this.dataInput2, this.dataInput3, this.dataInput4]
-        this.dataMax = Math.max(...checkTable);
-        this.dataMin = Math.min(...checkTable);
-        this.dataSum = this.dataInput1 + this.dataInput2 + this.dataInput3 + this.dataInput4;
-        this.dataAvg = this.dataSum/ 4;
+        this.dataAvg = document.querySelector('#avg')   
+
+        let checkTable: number[] = [data1, data2, data3, data4];
+        const max: number = Math.max(...checkTable);
+        const min: number = Math.min(...checkTable);
+        const sum: number = data1 + data2 + data3 + data4;
+        const avg: number = sum/ 4;    
+
+        this.setResult(this.dataMax, max);
+        this.setResult(this.dataMin, min);
+        this.setResult(this.dataSum, sum);
+        this.setResult(this.dataAvg, avg); 
+
     }
+    setResult(output: HTMLOutputElement, result: number) {
+        output.value = `${result}`;
+      }
 }
 
 const stats = new Stats();
-
-stats.run()
