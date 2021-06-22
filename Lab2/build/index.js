e7786a9f8074c19fa0dfa3064446fd660c4b9138
hprOne <bartek.laka@gmail.com>
2021-06-23 00:25:25 +0200

    Lab 2 - Completed

--- Lab2/build/index.js
+++ Lab2/build/index.js
@@ -1,11 +1,21 @@
 var clapSound;
 var boomSound;
+var kickSound;
+var openhatSound;
 var channel1 = [];
 appStart();
 function appStart() {
     window.addEventListener("keypress", onKeyDown);
     var btnPlayChannel1 = document.querySelector('#playChannel1');
+    var btnPlayA = document.querySelector('#Akey');
+    var btnPlayS = document.querySelector('#Skey');
+    var btnPlayD = document.querySelector('#Dkey');
+    var btnPlayF = document.querySelector('#Fkey');
     btnPlayChannel1.addEventListener('click', onPlayChannel);
+    btnPlayA.addEventListener('click', onBtnADown);
+    btnPlayS.addEventListener('click', onBtnSDown);
+    btnPlayD.addEventListener('click', onBtnDDown);
+    btnPlayF.addEventListener('click', onBtnFDown);
     getAudioTags();
 }
 function onPlayChannel() {
@@ -16,11 +26,33 @@ function onPlayChannel() {
 function getAudioTags() {
     boomSound = document.querySelector('[data-sound="boom"]');
     clapSound = document.querySelector('[data-sound="clap"]');
+    kickSound = document.querySelector('[data-sound="kick"]');
+    openhatSound = document.querySelector('[data-sound="openhat"]');
+}
+function onBtnADown() {
+    onKeyDown(new KeyboardEvent('key', { 'key': 'a' }));
+}
+function onBtnSDown() {
+    onKeyDown(new KeyboardEvent('key', { 'key': 's' }));
+}
+function onBtnDDown() {
+    onKeyDown(new KeyboardEvent('key', { 'key': 'd' }));
+}
+function onBtnFDown() {
+    onKeyDown(new KeyboardEvent('key', { 'key': 'f' }));
 }
 function onKeyDown(ev) {
-    // console.log(ev)
+    //console.log(ev)
     var key = ev.key;
     var time = ev.timeStamp;
+    if (ev.key === 'A')
+        document.getElementById("#Akey").click();
+    else if (ev.key === 'S')
+        document.getElementById("#Skey").click();
+    else if (ev.key === 'D')
+        document.getElementById("#Dkey").click();
+    else if (ev.key === 'F')
+        document.getElementById("#Fkey").click();
     channel1.push({ key: key, time: time });
     playSound(key);
 }
@@ -35,7 +67,15 @@ function playSound(key) {
             boomSound.currentTime = 0;
             boomSound.play();
             break;
+        case 'd':
+            kickSound.currentTime = 0;
+            kickSound.play();
+            break;
+        case 'f':
+            openhatSound.currentTime = 0;
+            openhatSound.play();
+            break;
     }
-    clapSound.currentTime = 0;
-    clapSound.play();
+    //clapSound.currentTime = 0;
+    //clapSound.play();
 }
--- Lab2/src/index.ts
+++ Lab2/src/index.ts
@@ -1,5 +1,7 @@
 let clapSound: HTMLAudioElement 
 let boomSound: HTMLAudioElement 
+let kickSound: HTMLAudioElement 
+let openhatSound: HTMLAudioElement 
 
 const channel1: any[] = [];
 appStart();
@@ -7,7 +9,15 @@ appStart();
 function appStart(): void{
     window.addEventListener("keypress", onKeyDown);
     const btnPlayChannel1 = document.querySelector('#playChannel1');
+    const btnPlayA = document.querySelector('#Akey');
+    const btnPlayS = document.querySelector('#Skey');
+    const btnPlayD = document.querySelector('#Dkey');
+    const btnPlayF = document.querySelector('#Fkey');
     btnPlayChannel1.addEventListener('click', onPlayChannel);
+    btnPlayA.addEventListener('click', onBtnADown);
+    btnPlayS.addEventListener('click', onBtnSDown);
+    btnPlayD.addEventListener('click', onBtnDDown);
+    btnPlayF.addEventListener('click', onBtnFDown);
     getAudioTags();
 }
 
@@ -20,13 +30,39 @@ function onPlayChannel(): void{
 function getAudioTags(){
     boomSound = document.querySelector('[data-sound="boom"]');
     clapSound = document.querySelector('[data-sound="clap"]');
+    kickSound = document.querySelector('[data-sound="kick"]');
+    openhatSound = document.querySelector('[data-sound="openhat"]');
+}
+
+function onBtnADown(){
+    onKeyDown(new KeyboardEvent('key',{'key':'a'}));
+}
+function onBtnSDown(){
+    onKeyDown(new KeyboardEvent('key',{'key':'s'}));    
+}
+function onBtnDDown(){
+    onKeyDown(new KeyboardEvent('key',{'key':'d'}));    
+}
+function onBtnFDown(){
+    onKeyDown(new KeyboardEvent('key',{'key':'f'}));   
 }
 
 function onKeyDown(ev: KeyboardEvent): void{
-    // console.log(ev)
+    //console.log(ev)
 
     const key = ev.key;
     const time = ev.timeStamp;
+    if(ev.key === 'A')
+        document.getElementById("#Akey").click(); 
+    else
+    if(ev.key === 'S')
+        document.getElementById("#Skey").click();
+    else
+    if(ev.key === 'D')
+        document.getElementById("#Dkey").click();
+    else
+    if(ev.key === 'F')
+        document.getElementById("#Fkey").click();
     channel1.push({key, time})
     playSound(key);
 }
@@ -34,15 +70,23 @@ function onKeyDown(ev: KeyboardEvent): void{
 function playSound(key: string) {
     // const soundKey = clapSound.dataset.key
     switch(key){
-        case 'a':
+        case 'a':           
             clapSound.currentTime = 0;
             clapSound.play();
             break;
-        case 's':
+        case 's': 
             boomSound.currentTime = 0;
             boomSound.play();
             break;
+        case 'd': 
+            kickSound.currentTime = 0;
+            kickSound.play();
+            break;
+        case 'f': 
+            openhatSound.currentTime = 0;
+            openhatSound.play();
+            break;
     }
-    clapSound.currentTime = 0;
-    clapSound.play();
+    //clapSound.currentTime = 0;
+    //clapSound.play();
 }
